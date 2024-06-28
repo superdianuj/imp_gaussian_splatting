9b11802c84f7294f0cb395378eaa2e9a5b71ff35
Spider Man <dianujkotov15@gmail.com>
2024-06-28 09:34:18 +0900

    Update resizer.py

--- SPSR/resizer.py
+++ SPSR/resizer.py
@@ -14,13 +14,13 @@ if os.path.exists(new_dir):
 os.system(f'mkdir {new_dir}')
         
 
-dir_list=sorted(os.listdir(dirr),key=lambda x: int(x.split('.')[0]))
+dir_list=sorted(os.listdir(dirr),key=lambda x: int(x.split('_')[-1].split('.')[0]) if '_' in x else int(x.split('.')[0]))
 im_paths=[os.path.join(dirr,curr_dir) for curr_dir in dir_list if curr_dir.endswith('png') or curr_dir.endswith('.jpg') or curr_dir.endswith('.JPG')]
 
 counter=0
 for pth in im_paths:
     img=cv2.imread(pth)
-    new_pth=os.path.join(new_dir,str(counter)+'.png')
+    new_pth=os.path.join(new_dir,'img_'+str(counter)+'.png')
     img_resized=cv2.resize(img,(550,550))
     cv2.imwrite(new_pth,img_resized)
     counter+=1
